b8a7a5e06e3ae1a98bb23367e144aa62a59dbb71
FormZet <leva200206@gmail.com>
2019-03-23 19:07:32 +0300

    Add files via upload

---
+++ boilerplate/test/assets/scripts/script.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+var question = ["Первый вопрос?",
+			"What do you think about your day??)))0",
+			"Вопрос",
+			"Вопрос",
+			"Вопрос",
+			"Вопрос",
+			"Вопрос",
+			"Вопрос",
+		" И последний вопрос"],
+
+
+
+      result = 0,
+      i = 0;
+quest.innerHTML = (i + 1) + " " + question[i];
+
+
+
+
+
+
+      
+answ.onchange = function() { 
+   result += +answ.value; 		
+   if( i < question.length - 1) {
+        answ.options[answ.options.selectedIndex].setAttribute("selected","");
+        i++;
+        quest.innerHTML = (i + 1) + " " + question[i];
+        answ.options[0].setAttribute("selected","selected");
+        answ.value = answ.options[0].text;
+   }
+   else if(result >= 20) {
+          res.innerHTML = result + " - Высокий Уровень";
+       	alert("Ты Кек"); 
+       window.location.href = "chooseTest.html";
+    }
+      else{
+       if(result < 20)  res.innerHTML = result + " - 	Низкий Уровень";
+       	alert("Ты НЕ ДОСТАТОЧНО ОБРАЗОВАН"); 
+       window.location.href = "../PhysicsB.html"; 
+   }
+}
+
+});	
